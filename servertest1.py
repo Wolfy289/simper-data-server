6d3df9d47a061efd6ae0b3a06bdfa6a73e6f78d7
Wolfy289 <134810740+Wolfy289@users.noreply.github.com>
2025-07-02 18:00:35 +0200

    Update servertest1.py

--- servertest1.py
+++ servertest1.py
@@ -1,51 +1,50 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, emit
+from flask_sock import Sock
 from datetime import datetime
 
-app = Flask(__name__, static_folder='static')
-socketio = SocketIO(app)
+app = Flask(__name__)
+sock = Sock(app)
+
+connected_clients = set()
 
 @app.route('/')
 def index():
-    return read_data("data.txt").replace("\n", "<br>")
+    return "<a href='/static/index.html'>text</a><br><a href='/api'>input</a>"
 
 @app.route('/api')
 def handle_data():
     data = request.args.get('data')
+    name = request.args.get('name')
+    data = name + "|" + data
     if data:
-        print(f"Ontvangen data: {data}")
-        add_data("data.txt", data)
+        print(f"{name,data}")
+        regel = f"\n{nu_time()}|{data}"
+
+        stuur_socket_data(regel)
+
         return str(data)
-    else:
-        return "?data=...."
+    return "<p>?name=...&data=...<p>"
 
-@socketio.on('connect')
-def handle_connect():
-    print("Client connected")
+@sock.route('/ws')
+def websocket_route(ws):
+    print("verbonden")
+    connected_clients.add(ws)
 
-def read_data(bestand):
-    with open(bestand, "r") as f:
-        inhoud = f.read()
-    return inhoud
+    try:
+        while True:
+            msg = ws.receive()
+    finally:
+        connected_clients.discard(ws)
 
-def add_data(bestand, data):
-    tijd = nu_time()
-    regel = "\n" + tijd + '|' + str(data)
-    with open(bestand, "a") as f:
-        f.write(regel)
-    # Zend naar alle websocket clients
-    socketio.emit('server_time', regel.replace("\n", "<br>"))
+
+def stuur_socket_data(data):
+    for client in connected_clients.copy():
+        client.send(data.replace("\n", "<br>"))
+
 
 def nu_time():
     nu = datetime.now()
-    dagen = nu.timetuple().tm_yday 
-    uren = nu.hour
-    minuten = nu.minute
-    seconden = nu.second
-    milliseconden = int(nu.microsecond / 10000)
-
-    return f"{dagen}:{uren:02}:{minuten:02}:{seconden:02}:{milliseconden:02}"
+    return f"{nu.timetuple().tm_yday}:{nu.hour:02}:{nu.minute:02}:{nu.second:02}:{int(nu.microsecond / 10000):02}"
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=8000)
-
+    app.run(debug=True, host='0.0.0.0', port=8000)
